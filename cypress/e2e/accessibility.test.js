c657069e2cea8bab0a725c4072c88ae01c8de738
MinhKhangTran <minhkhang.tran@gmx.de>
2021-11-10 06:17:22 +0100

    ✅ test a11y and hero

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -6,4 +6,12 @@ describe("Accessibility tests", () => {
   it("Has no detectable accessibility violations on load", () => {
     cy.checkA11y();
   });
+  it("Check mobile viewport for a11y", () => {
+    cy.viewport("iphone-8");
+    cy.checkA11y();
+  });
+  it("Check tablet viewport for a11y", () => {
+    cy.viewport("ipad-2");
+    cy.checkA11y();
+  });
 });
---
+++ cypress/e2e/hero.test.js
@@ -0,0 +1,48 @@
+/// <reference types="Cypress" />
+describe("Hero test", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("Hero desktop test", () => {
+    cy.viewport("macbook-13");
+    cy.get(".kasten").should("be.visible");
+    cy.get("h1.text-uppercase").should("be.visible");
+    cy.contains("Your local KDK club to reach your total goals").should(
+      "be.visible"
+    );
+    cy.get("button.mt-3").should("be.visible");
+    cy.get(".hero_text > .mt-5").should("be.visible");
+    cy.get(".d-flex.hero_socials").children().should("have.length", 3);
+    cy.get(
+      ".selected > .hero_image_wrapper > .gatsby-image-wrapper > div"
+    ).should("be.visible");
+  });
+  it("Hero mobile test", () => {
+    cy.viewport("macbook-13");
+    cy.get(".kasten").should("be.visible");
+    cy.get("h1.text-uppercase").should("be.visible");
+    cy.contains("Your local KDK club to reach your total goals").should(
+      "be.visible"
+    );
+    cy.get("button.mt-3").should("be.visible");
+    cy.get(".hero_text > .mt-5").should("be.visible");
+    cy.get(".d-flex.hero_socials").children().should("have.length", 3);
+    cy.get(
+      ".selected > .hero_image_wrapper > .gatsby-image-wrapper > div"
+    ).should("be.visible");
+  });
+  it("Hero tablet test", () => {
+    cy.viewport("macbook-13");
+    cy.get(".kasten").should("be.visible");
+    cy.get("h1.text-uppercase").should("be.visible");
+    cy.contains("Your local KDK club to reach your total goals").should(
+      "be.visible"
+    );
+    cy.get("button.mt-3").should("be.visible");
+    cy.get(".hero_text > .mt-5").should("be.visible");
+    cy.get(".d-flex.hero_socials").children().should("have.length", 3);
+    cy.get(
+      ".selected > .hero_image_wrapper > .gatsby-image-wrapper > div"
+    ).should("be.visible");
+  });
+});
